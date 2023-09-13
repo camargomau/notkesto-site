b99f212ca58bbb46c891cb6ba3b5f05478581de0
psycake <52770775+psycake@users.noreply.github.com>
2023-09-12 21:56:14 -0600

    Change format for tab titles

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -13,7 +13,7 @@ export default (() => {
 
     return (
       <head>
-        <title>{title}</title>
+        <title>{title+" - Notkesto"}</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta property="og:title" content={title} />
