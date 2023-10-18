fc8066f1a58d4fc61f78c7eb38aeb1cbe8c78e30
psycake <52770775+psycake@users.noreply.github.com>
2023-10-18 08:15:45 -0600

    Change dash in head titles to en dash

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -13,7 +13,7 @@ export default (() => {
 
     return (
       <head>
-        <title>{title+" - Notkesto"}</title>
+        <title>{title+" – Notkesto"}</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta property="og:title" content={title} />
